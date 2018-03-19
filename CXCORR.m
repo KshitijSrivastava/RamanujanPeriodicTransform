e7fe697453ae8ed81fb5c624ffe8af0eb7ff5c1e
KshitijSrivastava <kshitij127@yahoo.co.in>
2018-03-20 00:35:13 +0530

    Added few wav files, Added the reconstruction of signal, unused fn CXCORR.m added

---
+++ CXCORR.m
@@ -0,0 +1,10 @@
+function [x,c]=CXCORR(a,b)
+na=norm(a);
+nb=norm(b); a=a/na; %normalization 
+b=b/nb;
+for k=1:length(b)
+    c(k)=a*b';
+    b=[b(end),b(1:end-1)];
+    %circular shift
+end x=[0:length(b)-1]; %lags
+end
