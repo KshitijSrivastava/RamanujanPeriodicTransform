4ddf630aeea72d289291f41919b16dff285d7d4f
KshitijSrivastava <kshitij127@yahoo.co.in>
2018-03-19 13:19:13 +0530

    Added more signals & modification in the making B matrix (2^N) rule

--- period_estimator.m
+++ period_estimator.m
@@ -1,9 +1,13 @@
+clc;
+clear all;
+close all;
 %x=[1,1,1,1]; %Taking the input
-filename='500hz_0.8Amp_SineWave.wav';%Name of the file
+filename='200Hz_0.8Amp_Sine&Gauss-noise-0.1Amp.wav';%Name of the file
 [myrec,fs] = audioread(filename);%Reading the files
 myrecording = myrec(:,1);%Taking only one sample of the test
-x=myrecording;
-
+x=myrecording(1:256,1);
+x=transpose(x);
+x=xcorr(x);
 x_length=length(x);%Finding the length of the input
 sound(x)
 %%
@@ -20,12 +24,17 @@ q_length=length(q);%Finding the length of q matrix
 
 mat=zeros(x_length,1);%Initial Matrix made
 
+q_matrix=zeros(x_length,1);
+count=1;
 for i=1:q_length %For looping through the various q's
    phi=phi_q(q(i));%Finding the phi(q)
    
    for l=0:phi-1  %Looping through 0 to phi(q)-1
     Cqn_l=Cqnl(q(i),x_length,l); %Outputing one column of the matrix
-    mat=horzcat(mat,Cqn_l); %Horizontal catenation  of the column with the matrix    
+    
+    q_matrix(count,1)=q(i);
+    count=count+1;
+    mat=horzcat(mat,Cqn_l); %Horizontal catenation  of the column with the matrix
    end
     
 end
@@ -33,4 +42,7 @@ end
 mat=mat(:,2:x_length+1);%Taking the 2nd column to the last column of the matrix 
 mat=round(mat);%Rounding off the matrix
 
-B_mat=inv(mat)*transpose(x); %Finding the B matrix
+B_mat=(mat.')*transpose(x); %Finding the B matrix
+mdfdCoeff = B_mat.*diag(sqrt(inv(mat.'*mat)));
+figure;loglog(abs(mdfdCoeff));title('Normalized Coefficients');
+hold on;loglog(q_matrix,'r');
